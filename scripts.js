2dd9ccb8a609046eb541f8271b197c40435e277c
ErikBoesen <me@erikboesen.com>
2016-08-16 13:33:29 -0400

    Dark blue theme

--- scripts.js
+++ scripts.js
@@ -8,31 +8,73 @@ function initMap() {
 		},
 		zoom: 2,
 		styles: [{
-			featureType: 'all',
+			featureType: "water",
+			elementType: "geometry",
 			stylers: [{
-				brightness: 20
+				color: "#193341"
 			}]
 		}, {
-			featureType: 'road.arterial',
-			elementType: 'geometry',
+			featureType: "landscape",
+			elementType: "geometry",
 			stylers: [{
-				hue: '#00ffee'
+				color: "#2c5a71"
+			}]
+		}, {
+			featureType: "road",
+			elementType: "geometry",
+			stylers: [{
+				color: "#29768a"
 			}, {
-				saturation: 50
+				lightness: -37
 			}]
 		}, {
-            featureType: 'poi.school',
-            // TODO: Add special styling for schools
-        }, {
-            featureType: 'water',
-            stylers: [{
-                hue: '#333333'
-            }, {
-                saturation: -100
-            }, {
-                lightness: -50
-            }]
-        }]
+			featureType: "poi",
+			elementType: "geometry",
+			stylers: [{
+				color: "#406d80"
+			}]
+		}, {
+			featureType: "transit",
+			elementType: "geometry",
+			stylers: [{
+				color: "#406d80"
+			}]
+		}, {
+			elementType: "labels.text.stroke",
+			stylers: [{
+				visibility: "on"
+			}, {
+				color: "#3e606f"
+			}, {
+				weight: 2
+			}, {
+				gamma: 0.84
+			}]
+		}, {
+			elementType: "labels.text.fill",
+			stylers: [{
+				color: "#ffffff"
+			}]
+		}, {
+			featureType: "administrative",
+			elementType: "geometry",
+			stylers: [{
+				weight: 0.6
+			}, {
+				color: "#1a3541"
+			}]
+		}, {
+			elementType: "labels.icon",
+			stylers: [{
+				visibility: "off"
+			}]
+		}, {
+			featureType: "poi.park",
+			elementType: "geometry",
+			stylers: [{
+				color: "#2c5a71"
+			}]
+		}]
 	});
 	for (i = 0; i < teams.length; i++) {
 		var marker = new google.maps.Marker({
