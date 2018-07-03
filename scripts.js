b9893e9a2b0dbbf26f87c1de6c27943a2fc34dac
ErikBoesen <me@erikboesen.com>
2018-07-03 17:06:13 -0400

    Clean up scripts.js

--- scripts.js
+++ scripts.js
@@ -6,13 +6,11 @@
 var map;
 var markers = [];
 
-// Start all markers in displayable state (not hidden)
+// Show all markers
 var state = {T: true, R: true, D: true, C: true};
 
 function initMap() {
-// Set up the google map parameters and options
-// last few lines of function create team and event markers
-
+    // Initialize Google Map
     map = new google.maps.Map(document.getElementById('map'), {
         center: {
             lat: 30,
@@ -131,15 +129,9 @@ function initMap() {
         ]
     });
 
-// Create team and event markers
-
-    for (i = 0; i < teamInfo.length; i++)
-        createTeamMarker(teamInfo[i]);
-
-    for (i = 0; i < events.length; i++)
-        createEventMarker(events[i]);
-
-// And start application
+    // Create team and event markers
+    for (i = 0; i < teamInfo.length; i++) createTeamMarker(teamInfo[i]);
+    for (i = 0; i <   events.length; i++) createEventMarker(events[i]);
 
     addKeyboardListener();
 }
@@ -150,12 +142,10 @@ function createEventMarker(eventEntry) {
             lat: eventEntry.lat,
             lng: eventEntry.lng
         };
-        var key = eventEntry.key;
 
         if (position.lat && position.lng) {
             var image = {
-                url: 'resources/img/' + (eventEntry.type=='R' ? 'regional' :
-                     (eventEntry.type == 'C' ? 'championship' : 'district')) + '.png',
+                url: 'resources/img/' + (eventEntry.type == 'R' ? 'regional' : (eventEntry.type == 'C' ? 'championship' : 'district')) + '.png',
                 scaledSize: new google.maps.Size(30, 30)
             };
 
@@ -168,7 +158,7 @@ function createEventMarker(eventEntry) {
                 type: eventEntry.type
             });
 
-            google.maps.event.addListener(marker, 'click', function() {
+            google.maps.event.addListener(marker, 'click', function () {
                 openInfo(marker)
             });
 
@@ -189,19 +179,17 @@ function createTeamMarker(teamInfo) {
             };
         } else {
             position = {
-                lat: teamInfo.lat + (Math.random()-.5) / 50,
-                lng: teamInfo.lng + (Math.random()-.5) / 50
+                lat: teamInfo.lat + (Math.random() - .5) / 50,
+                lng: teamInfo.lng + (Math.random() - .5) / 50
             };
         }
         var custom = icons.indexOf(title) !== -1;
         var imageUrl = 'resources/img/marker.png';
         if (custom) {
-          imageUrl = 'logos/' + title + '.png';
-        } else {
-          if (teamAvatars[title]) {
+            imageUrl = 'logos/' + title + '.png';
+        } else if (teamAvatars[title]) {
             custom = true;
             imageUrl = 'data:image/png;base64,' + teamAvatars[title]["img"];
-          }
         }
 
         var image = {
@@ -218,8 +206,8 @@ function createTeamMarker(teamInfo) {
             type: 'T'
         });
 
-        google.maps.event.addListener(marker, 'click', function() {
-            openInfo(marker)
+        google.maps.event.addListener(marker, 'click', function () {
+            openInfo(marker);
         });
 
         markers.push(marker);
@@ -228,72 +216,60 @@ function createTeamMarker(teamInfo) {
 
 function openInfo(marker) {
     var req = new XMLHttpRequest();
-
-    req.open('GET', 'https://www.thebluealliance.com/api/v3/' +
-            (marker.type == 'T' ? 'team/' : 'event/') +
-            marker.key + '?X-TBA-Auth-Key=' +
-            'VCZM2oYCpR1s3OHxFbjdVQrtkk0LY1wcvyhH8hiNrzm1mSQnUn1t9ZDGyTqN4Ieq'
-    );
+    req.open('GET', 'https://www.thebluealliance.com/api/v3/' + (marker.type == 'T' ? 'team/' : 'event/') + marker.key + '?X-TBA-Auth-Key=VCZM2oYCpR1s3OHxFbjdVQrtkk0LY1wcvyhH8hiNrzm1mSQnUn1t9ZDGyTqN4Ieq');
     req.send();
     req.onreadystatechange = function() {
-      if (req.readyState === 4 && req.status === 200) {
-        var parsed = JSON.parse(req.responseText);
+        if (req.readyState === 4 && req.status === 200) {
+            var parsed = JSON.parse(req.responseText);
+            var content = '';
 
-        var content = '';
+            if (marker.type == 'T') {
+                content += '<h1>';
+                content += parsed.website ? '<a href="' + parsed.website + '">' : '';
+                content += 'Team ' + parsed.team_number;
+                content += parsed.nickname ? ' - ' + parsed.nickname : '';
+                content += parsed.website ? '</a></h1>' : '</h1>';
 
-        if (marker.type == 'T') {
-            content += '<h1>';
-            content += parsed.website ? '<a href="' + parsed.website + '">' : '';
-            content += 'Team ' + parsed.team_number;
-            content += parsed.nickname ? ' - ' + parsed.nickname : '';
-            content += parsed.website ? '</a></h1>' : '</h1>';
-
-            content += parsed.motto ? '<p><em>"' + parsed.motto + '"</em></p>' : '';
-            content += '<ul>';
-            content += '<li><strong>Location:</strong> ' + parsed.city + ', ';
-            content += parsed.state_prov + ' ' + parsed.postal_code + ', ';
-            content += parsed.country + '</li>';
-            content += parsed.rookie_year
-                ? '<li><strong>Rookie year:</strong> ' + parsed.rookie_year + '</li>'
-                : '';
-            content +=
-                '<li><a href="http://thebluealliance.com/team/' + parsed.team_number +
-                '">View on The Blue Alliance</a></li>';
-            content += '</ul>';
-        } else {
-            if (parsed.event_type == 4) {
-                content +=  '<h1>FIRST Championship ' + parsed.city + '</h1>';
+                content += parsed.motto ? '<p><em>"' + parsed.motto + '"</em></p>' : '';
+                content += '<ul>';
+                content += '<li><strong>Location:</strong> ' + parsed.city + ', ';
+                content += parsed.state_prov + ' ' + parsed.postal_code + ', ';
+                content += parsed.country + '</li>';
+                content += parsed.rookie_year ? '<li><strong>Rookie year:</strong> ' + parsed.rookie_year + '</li>' : '';
+                content += '<li><a href="http://thebluealliance.com/team/' + parsed.team_number + '">View on The Blue Alliance</a></li>';
+                content += '</ul>';
             } else {
-                content += '<h1>' + parsed.short_name + '</h1>';
+                if (parsed.event_type == 4) {
+                    content += '<h1>FIRST Championship ' + parsed.city + '</h1>';
+                } else {
+                    content += '<h1>' + parsed.short_name + '</h1>';
+                }
+
+                content += '<ul>';
+                if (parsed.event_type_string.startsWith('District')) {
+                    content += '<li><strong>District:</strong> ' + parsed.district.abbreviation + '</li>';
+                }
+                if (parsed.week) {
+                    content += '<li><strong>Week:</strong> ' + parsed.week + '</li>';
+                }
+                var start = new Date(parsed.start_date).toLocaleDateString();
+                var end = new Date(parsed.end_date).toLocaleDateString();
+                content += '<li><strong>Date:</strong> ' + start + ' thru ' + end + '</li>';
+                content += '<li><a href="http://www.thebluealliance.com/event/' + marker.key + '">View on The Blue Alliance</a></li>';
+                content += '</ul>';
             }
 
-            content += '<ul>';
-            if (parsed.event_type_string.startsWith('District')) {
-                content += '<li><strong>District:</strong> ' +
-                            parsed.district.abbreviation + '</li>';
-            }
-            if (parsed.week) {
-                content += '<li><strong>Week:</strong> ' + parsed.week + '</li>';
-            }
-            var start = new Date(parsed.start_date).toLocaleDateString();
-            var end = new Date(parsed.end_date).toLocaleDateString();
-            content += '<li><strong>Date:</strong> ' + start + ' thru ' + end + '</li>';
-            content += '<li><a href="http://www.thebluealliance.com/event/' +
-                        marker.key + '">View on The Blue Alliance</a></li>';
-            content += '</ul>'
+            try {
+                var oldInfoWindow = document.getElementsByClassName('gm-style-iw')[0];
+                oldInfoWindow.parentNode.parentNode.removeChild(oldInfoWindow.parentNode);
+            } catch (e) {}
+
+            var infoWindow = new google.maps.InfoWindow({
+                content: content
+            });
+
+            infoWindow.open(map, marker);
         }
-
-        try {
-            var oldInfoWindow = document.getElementsByClassName('gm-style-iw')[0];
-            oldInfoWindow.parentNode.parentNode.removeChild(oldInfoWindow.parentNode);
-        } catch (e) {}
-
-        var infoWindow = new google.maps.InfoWindow({
-            content: content
-        });
-
-        infoWindow.open(map, marker);
-      }
     }
 }
 
@@ -325,16 +301,16 @@ function toggleMarkers(type) {
             newMap = state.C ? map : null;
             change = 'C';
             break;
-     }
-     for (i = 0; i < markers.length; i++) {
-         if (markers[i].type == change) {
-              markers[i].setMap(newMap);
-         }
+    }
+    for (i = 0; i < markers.length; i++) {
+        if (markers[i].type == change) {
+            markers[i].setMap(newMap);
+        }
     }
 }
 
 function addKeyboardListener() {
-    document.addEventListener('keyup', function(event) {
+    document.addEventListener('keyup', function (event) {
         switch (event.keyCode) {
             // Esc
             case 27:
@@ -365,17 +341,17 @@ function addKeyboardListener() {
 }
 
 function openAbout() {
-   document.getElementById('about').style.display='inline-block';
+    document.getElementById('about').style.display = 'inline-block';
 }
 
 function closeAbout() {
-   document.getElementById('about').style.display='none';
+    document.getElementById('about').style.display = 'none';
 }
 
 function toggleAbout() {
-   if (document.getElementById('about').style.display == 'none') {
-      openAbout();
-   } else {
-      closeAbout();
-   }
+    if (document.getElementById('about').style.display == 'none') {
+        openAbout();
+    } else {
+        closeAbout();
+    }
 }
