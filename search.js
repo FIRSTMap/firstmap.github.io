78f771e2fbf55ab3add356c39b8284aada8449e4
FishDog5000 <fishdog5000@gmail.com>
2019-07-08 22:05:12 -0400

    Add searching and filtering
    * Add search menu (opens with 's'). While the serach menu is open,
      toggle keys (like pressing 't' to toggle teams) are not enabled
      because otherwise stuff starts toggling (including the search menu
      itself) when the user starts to type in the search box. Teams,
      events, etc. can still be toggled from the buttons at the top of
      the page.
    * Move team marker creation to before event marker creation to
      guarantee that event markers are created _after_ team markers.
    * Created openMarkerInfo function to remove some duplicate code
    * Created function updateVisibleMarkers out of toggleMarkers and renamed
      updateMarkerVisibility to updateMarkerVisibilityParam to reduce
      confusion between the two. The updateVisibleMarkers function updates
      what markers are visible taking into account the markers in
      markers.filtered, if it is not null (markers.filtered is null when
      no filter is applied, but has the marker keys and values to show when
      a filter is applied).

---
+++ search.js
@@ -0,0 +1,356 @@
+'use strict';
+
+// Filter / search user-interface stuff
+var searchBar = document.getElementById('searchInput');
+var filterBarTeams = document.getElementById('filterInputTeams');
+var filterBarOther = document.getElementById('filterInputOther');
+var filterBar;
+var searchType = document.getElementById('searchType');
+var filterType = document.getElementById('filterType');
+var searchInfoText = document.getElementById('searchInfoText');
+var filterInfoText = document.getElementById('filterInfoText');
+
+function updateFilterType() {
+    var type = filterType.value;
+
+    if (type === 'team') {
+        document.getElementById('teamFilter').hidden = false;
+        document.getElementById('otherFilter').hidden = true;
+        filterBar = filterBarTeams;
+    } else {
+        document.getElementById('teamFilter').hidden = true;
+        document.getElementById('otherFilter').hidden = false;
+        filterBar = filterBarOther;
+
+
+        while (filterBarOther.firstChild) {
+            filterBarOther.removeChild(filterBarOther.firstChild);
+        }
+
+        let option = document.createElement('option');
+        option.value = '';
+        option.text = 'None';
+        filterBarOther.appendChild(option);
+
+        var options = [];
+
+        if (type === 'event') {
+            // Add all events to dropdown
+
+            for (let eventKey in eventData) {
+                if (eventData.hasOwnProperty(eventKey)) {
+                    let event = eventData[eventKey];
+
+                    option = document.createElement('option');
+                    option.value = event.key;
+                    option.text = event.name + ' [' + event.key + ']';
+
+                    options.push(option);
+                }
+            }
+        } else {
+            // Add all districts to dropdown
+            for (let district of districtList) {
+                option = document.createElement('option');
+                option.value = district.key;
+                option.text = district.display_name + ' [' + district.key + ']';
+
+                options.push(option);
+            }
+        }
+
+        // Sort all the options alphabetically
+        options.sort((a, b) => a.text.localeCompare(b.text));
+
+        for (let option of options) {
+            filterBarOther.appendChild(option);
+        }
+    }
+}
+
+function clearFilterClick() {
+    filterBar.value = '';
+    clearFilter();
+}
+
+function inputKeyUp(input) {
+    if (event.key === "Enter") {
+        if (input === filterBarTeams) {
+            filter();
+        } else if (input === searchBar) {
+            search();
+        }
+    }
+}
+
+// Filtering and searching code
+
+function filterToParam() {
+    var filterKey = params.get('filter');
+
+    if (filterKey && filterKey.length > 2) {
+        var type = '';
+        filterKey = filterKey.toLowerCase();
+
+        if (filterKey.startsWith('t-')) {
+            type = 'team';
+        } else if (filterKey.startsWith('e-')) {
+            type = 'event';
+        } else if (filterKey.startsWith('d-')) {
+            type = 'district';
+        } else {
+            clearFilter();
+            return;
+        }
+
+        var key = filterKey.slice(2);
+
+        filterType.value = type;
+        updateFilterType();
+        filterBar.value = key;
+        
+        filter(key, type, function(success) {
+            if (!success) {
+                clearFilter();
+            }
+        });
+    }
+}
+
+function setFilterParam(type, query) {
+    var filter;
+
+    if (type == 'team') {
+        filter = 't-';
+    } else if (type == 'event') {
+        filter = 'e-';
+    } else if (type == 'district') {
+        filter = 'd-';
+    }
+
+    if (!filter || !query) {
+        params.delete('filter');
+    } else {
+        filter += query;
+        params.set('filter', filter);
+    }
+
+    pushHistory();
+}
+
+function search(query, type) {
+    if (!query && !type) {
+        query = searchBar.value;
+        type = searchType.value;
+    }
+
+    if (!query) {
+        searchInfoText.innerText = 'Please enter search text.';
+        return;
+    }
+
+    query = query.toLowerCase();
+
+    var key;
+
+    if (type === 'team') {
+        key = 'frc' + query;
+    } else if (type === 'event') {
+        key = query;
+    } else {
+        return false;
+    }
+    
+    var marker = markers.keys[key];
+
+    if (marker) {
+        if (markers.filtered && !markers.filtered[key]) {
+            searchInfoText.innerText = 'Error: ' + type + ' \'' + query + '\' not found within filtered area.';
+            return;
+        }
+
+        openMarkerInfo(marker);
+        searchInfoText.innerText = '';
+    } else {
+        searchInfoText.innerText = 'Error: ' + type + ' \'' + query + '\' not found.';
+    }
+}
+
+function failFilter(error) {
+    filterInfoText.innerText = error;
+    console.log(error);
+    clearFilter();
+}
+
+function filter(query, type) {
+    if (!query && !type) {
+        query = filterBar.value;
+        type = filterType.value;
+    }
+
+    if (!query) {
+        clearFilter();
+        return;
+    }
+
+    query = query.toLowerCase();
+
+    if (type === 'team') {
+        // Show all the events a team is attending / attended in the current season
+        let teamKey = 'frc' + query;
+        let marker = markers.keys[teamKey];
+
+        getTBAQuery('/team/' + teamKey + '/events/' + CURRENT_YEAR + '/keys', function(events, err) {
+            if (err && !events) {
+                failFilter('Error: team \'' + query + '\' not found.');
+                return;
+            }
+
+            markers.filtered = {};
+
+            // Team 9999 isn't on the map (since it's an offseason demo team)
+            // but someone might still want to see what events it "attended,"
+            // so I guess I'll just check if the marker exists here instead of
+            // earlier.
+            if (marker) {
+                markers.filtered[teamKey] = marker;
+            }
+
+            events.forEach(key => {
+                let parent = eventData[key].parent_event_key;
+
+                if (parent) {
+                    markers.filtered[parent] = markers.keys[parent];
+                } else {
+                    markers.filtered[key] = markers.keys[key];
+                }
+            });
+
+            setFilterParam('team', query);
+            filterInfoText.innerText = '';
+
+            updateVisibleMarkers();
+            zoomFitVisible();
+        });
+    } else if (type === 'event') {
+        // Show all the teams attending an event. This works for event divisions too.
+        // If called with a parent event, all teams from all divisions are shown.
+        let event = eventData[query];
+        
+        // If the event exists
+        if (event) {
+            function processTeams(teams) {
+                markers.filtered = {};
+
+                let parent = event.parent_event_key;
+
+                if (parent) {
+                    markers.filtered[parent] = markers.keys[parent];
+                } else {
+                    markers.filtered[query] = markers.keys[query];
+                }
+
+                teams.forEach(key => {
+                    markers.filtered[key] = markers.keys[key];
+                });
+
+                setFilterParam('event', query);
+                filterInfoText.innerText = '';
+
+                updateVisibleMarkers();
+                zoomFitVisible();
+            }
+
+            let teamList;
+
+            getTBAQuery('/event/' + query + '/teams/keys', function(teams, err) {
+                if (err && !teams) {
+                    failFilter('Error filtering to event with key \'' + query + '\'');
+                    return;
+                }
+
+                teamList = teams;
+
+                if (event.division_keys && event.division_keys.length > 0) {
+                    let i = 0;
+
+                    function getNextDiv() {
+                        getTBAQuery('/event/' + event.division_keys[i] + '/teams/keys', function(teams, err) {
+                            if (err && !teams) {
+                                failFilter('Error: failure to get teams for event division \'' + + event.division_keys[i] + '\'');
+                                return;
+                            }
+
+                            i++;
+                            teamList = teamList.concat(teams);
+
+                            if (i >= event.division_keys.length) {
+                                processTeams(teamList);
+                            } else {
+                                getNextDiv();
+                            }
+                        });
+                    }
+                    getNextDiv();
+                } else {
+                    processTeams(teamList);
+                }
+            });
+        } else {
+            failFilter('Error: event with key \'' + query + '\' not found.');
+            return;
+        }
+    } else if (type === 'district') {
+        getTBAQuery('/district/' + query + '/teams/keys', function(teams, err) {
+            if (err && !teams) {
+                failFilter('Error: district with key \'' + query + '\' not found.');
+                return;
+            }
+
+            getTBAQuery('/district/' + query + '/events/keys', function(events, err) {
+                if (err && !events) {
+                    failFilter('Error: failure to get events for district \'' + query + '\'');
+                    return;
+                }
+
+                var keys = teams.concat(events);
+
+                markers.filtered = {};
+    
+                keys.forEach(key => {
+                    // Event divisions do not have markers, so
+                    // don't include them in the filter list
+                    if (markers.keys[key]) {
+                        markers.filtered[key] = markers.keys[key];
+                    }
+                });
+
+                setFilterParam('district', query);
+                filterInfoText.innerText = '';
+    
+                updateVisibleMarkers();
+                zoomFitVisible();
+            });
+        });
+    }
+}
+
+function clearFilter() {
+    markers.filtered = null;
+    setFilterParam('none', '');
+
+    updateVisibleMarkers();
+}
+
+function zoomFitVisible() {
+    var bounds = new google.maps.LatLngBounds();
+
+    for (var marker of markers.all) {
+        if (marker.getVisible()) {
+            bounds.extend(marker.getPosition());
+        }
+    }
+
+    // Add minimum 30 pixel border
+    map.fitBounds(bounds, 30);
+}
